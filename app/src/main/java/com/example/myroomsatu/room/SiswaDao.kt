a1411d3f7b8fb9251933b700896629fdf34c84b5
Asti22 <uun3461@gmail.com>
2025-11-27 09:44:16 +0700

    Refactor SiswaDao formatting and query cleanup

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -8,7 +8,8 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT *from tblSiswa ORDER BY nama ASC")
+
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
