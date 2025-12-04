8b7cac6883dd3c6e92772d0b23656214867e45a8
Asti22 <uun3461@gmail.com>
2025-12-04 11:04:12 +0700

    menambhkan 3 fungsi get,update,delete

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -1,20 +1,29 @@
-package com.example.myroomsatu.room
+package com.example.myroomsisma.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.myroomsatu.data.Siswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-
-    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
-    @Query(value = "SELECT * from tblSiswa WHERE id= :id")
-    fun getSiswa(id:Int): Flow<Siswa>
+
+    //Edit : tambah 3 fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
